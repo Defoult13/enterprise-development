8dc0dacada766c53ac219d9c8914beeb34835d07
aidenpirs007@mail.ru <aidenpirs007@mail.ru>
2025-10-14 13:43:07 +0400

    Added property types and purposes

---
+++ RealEstateAgency/RealEstate.Domain/Models/PropertyPurpose.cs
@@ -0,0 +1,16 @@
+﻿namespace RealEstate.Domain.Models;
+
+/// <summary>
+/// The purpose of a property
+/// </summary>
+public enum PropertyPurpose
+{
+    /// <summary>
+    /// Property intended for commercial use
+    /// </summary>
+    Commercial,
+    /// <summary>
+    /// Property intended for residential use
+    /// </summary>
+    Residential
+}
